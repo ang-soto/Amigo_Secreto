a01b16826160e6eb5fb12b6e5102ca55c594489f
ang-soto <140351048+ang-soto@users.noreply.github.com>
2025-03-05 20:07:09 -0600

    Update app.js
    Funcion agregar amigo

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
+
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value.trim();
+    
+    if (nombreAmigo === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+    
+    if (listaAmigos.includes(nombreAmigo)) {
+        alert("Este nombre ya fue agregado.");
+        return;
+    }
+    
+    listaAmigos.push(nombreAmigo);
+    actualizarListaAmigos();
+    inputAmigo.value = "";
+}
